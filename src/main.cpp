0ec050507fa40907482ee34c110e84e056131483
Aidan Ih <aidan.h.ih@gmail.com>
2025-01-27 21:57:48 -0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+ 	
+#include <iostream>
+#include "vanEmdeBoas.cpp"
+
+int main ()
+{
+  std::cout << "Hello World!\n";
+
+  
+
+
+}
+
---
+++ src/vanEmdeBoas.cpp
@@ -0,0 +1,11 @@
+ 	
+#include <iostream>
+
+class VanEmdeBoas() {
+  private:
+  int UNIVERSE_SIZE = 2 << 32;
+
+  public: 
+  int insert(int number);
+  int successor;
+}
