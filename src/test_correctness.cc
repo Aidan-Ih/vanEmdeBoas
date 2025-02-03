78d9edd2bdffabd04d7e06eb275a8b7a689c9cf3
Aidan Ih <aidan.h.ih@gmail.com>
2025-02-03 01:54:16 -0500

    testing changes

--- src/test_correctness.cc
+++ src/test_correctness.cc
@@ -46,26 +46,18 @@ int main(int argc, char** argv) {
 	safe_rand_bytes((unsigned char *)out_numbers, sizeof(*out_numbers) * N);
 
 	// Create a bst using std::set
-	std::set<uint32_t> bst;
 	high_resolution_clock::time_point t1, t2;
-
+    std::set<uint32_t> bst;
   	VanEmdeBoas veb = VanEmdeBoas(32);
- 
-	//Insert N items from in_numbers
+
+    //Insert N items from in_numbers
 	t1 = high_resolution_clock::now();
 	for (uint32_t i = 0; i < N; ++i) {
 		bst.insert(in_numbers[i]);
 	}
 	t2 = high_resolution_clock::now();
 	std::cout << "BST Time to insert " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
-
-	t1 = high_resolution_clock::now();
-	for (uint32_t i = 0; i < N; ++i) {
-		veb.insert(in_numbers[i]);
-	}
-	t2 = high_resolution_clock::now();
-	std::cout << "VEB Time to insert " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
- 
+	
 
 	// Query N items from in_numbers
 	t1 = high_resolution_clock::now();
@@ -79,6 +71,16 @@ int main(int argc, char** argv) {
 	t2 = high_resolution_clock::now();
 	std::cout << "BST Time to query " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
 
+	// N Successor queries from out_numbers
+	// Validate correctness of successor operations by comparing outputs
+ 
+	t1 = high_resolution_clock::now();
+	for (uint32_t i = 0; i < N; ++i) {
+		veb.insert(in_numbers[i]);
+	}
+	t2 = high_resolution_clock::now();
+	std::cout << "VEB Time to insert " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
+
 	t1 = high_resolution_clock::now();
 	for (uint32_t i = 0; i < N; ++i) {
 		bool ret = veb.query(in_numbers[i]);
@@ -90,11 +92,8 @@ int main(int argc, char** argv) {
 	t2 = high_resolution_clock::now();
 	std::cout << "VEB Time to query " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
 
-	// N Successor queries from out_numbers
-	// Validate correctness of successor operations by comparing outputs
 	
-
-	/*
+    //check that all successor queries result in the same output
 	for (uint32_t i = 0; i < N; ++i) {
 		auto bst_ret = bst.lower_bound(out_numbers[i]);
 		long veb_ret = veb.successor(out_numbers[i]);
@@ -112,43 +111,14 @@ int main(int argc, char** argv) {
 				bst_ret = bst.lower_bound(out_numbers[i] + 1);
 				if (*bst_ret != veb_ret) {
 					std::cerr << "successor operations returned different values:\n Query: " + std::to_string(out_numbers[i]) + " BST: " + std::to_string(*bst_ret) + " VEB: " + std::to_string(veb_ret) + "\n";
+                    exit(0);
 				}
 			}
 		}	 
 	}
-	*/
-		
-	t1 = high_resolution_clock::now();
-    for (uint32_t i = 0; i < N; ++i) {
-        auto ret = bst.lower_bound(out_numbers[i]);
-        if (ret != bst.end() && *ret < out_numbers[i]) {
-            std::cerr << "successor query in BST failed. Item: " + std::to_string(out_numbers[i]) + " Successor: " + std::to_string(*ret) + "\n";
-            exit(0);
-        }
-    }
-    t2 = high_resolution_clock::now();
-    std::cout << "Time to successor query " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
-	  
-	/*
+
+    std::cout << "Success!\n";
 	
-	
-	t1 = high_resolution_clock::now();
-	for (uint32_t i = 0; i < N; ++i) {
-		auto bst_ret = bst.lower_bound(out_numbers[i]);
-		*bst_ret;
-	}
-
-	t2 = high_resolution_clock::now();
-	std::cout << "BST Time to successor query " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
-*/
-	t1 = high_resolution_clock::now();
-	for (uint32_t i = 0; i < N; ++i) {
-		auto veb_ret = veb.successor(out_numbers[i]);
-	}
-
-	t2 = high_resolution_clock::now();
-	std::cout << "VEB Time to successor query " + std::to_string(N) + " items: " + std::to_string(elapsed(t1, t2)) + " secs\n";
-
 	return 0;
 } 
 
